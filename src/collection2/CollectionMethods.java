2f24d478f097d17b1b0f96c0e13cd40dd697f1c4
srinivas <srinivaskarre@rocketmail.com>
2019-12-27 18:57:21 +0530

    new change

--- src/collection2/CollectionMethods.java
+++ src/collection2/CollectionMethods.java
@@ -40,6 +40,7 @@ public class CollectionMethods {
 		
 		System.out.println("final arraylist is="+a);
 		System.out.println("again arraylist is="+a);
+		System.out.println("new modification");
 	}
 
 }
